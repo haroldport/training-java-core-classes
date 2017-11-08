3398dfaea13358bbab8378af95871339f5bed26b
Harold Portocarrero <hportocarrero@todo1.com>
2017-11-08 11:46:03 -0500

    ejemplo usando la clase Period para determinar tiempos entre fechas

--- src/com/arquitecturajava/ejemplo009/Principal.java
+++ src/com/arquitecturajava/ejemplo009/Principal.java
@@ -1,13 +1,14 @@
 package com.arquitecturajava.ejemplo009;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
 		LocalDate miFecha = LocalDate.now();
-		LocalDate miFecha2 = LocalDate.of(2016, 10, 10);
+		LocalDate miFecha2 = LocalDate.of(2016, 5, 10);
 		System.out.println(miFecha);
 		System.out.println(miFecha2);
 		System.out.println(miFecha.getMonth());
@@ -22,6 +23,12 @@ public class Principal {
 //		LocalDate miFecha6 = miFecha5.plusMonths(1);
 		//ejemplo de API fluida en Java 8
 		System.out.println(miFecha4.plusYears(1).plusMonths(1).plusDays(10));
+		
+		Period periodo = Period.between(miFecha2, miFecha);
+		System.out.println(periodo.getYears());
+		System.out.println(periodo.getMonths());
+		System.out.println(periodo.getDays());
+		System.out.println(periodo.toTotalMonths());
 		
 	}
 
