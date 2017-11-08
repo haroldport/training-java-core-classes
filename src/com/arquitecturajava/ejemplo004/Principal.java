cbf34e4639eaa7bd59dfee363f6aa87c1b7b63eb
Harold Portocarrero <hportocarrero@todo1.com>
2017-11-08 08:36:26 -0500

    ejemplo con la clase Math

---
+++ src/com/arquitecturajava/ejemplo004/Principal.java
@@ -0,0 +1,14 @@
+package com.arquitecturajava.ejemplo004;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.pow(2, 2));
+		System.out.println(Math.round(6.2453));
+		System.out.println(Math.sqrt(16));
+		
+	}
+
+}
